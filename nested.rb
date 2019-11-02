728683bdf5af0efecb60406685fddc9ee4c3fbad
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-02 09:05:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,8 +59,10 @@ def dennis_ritchies_language
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages] = "C"
 		 programmer_hash[:dennis_ritchie][:languages]
+
+		 #programmer_hash[:dennis_ritchie][:languages] = "C"
+		 #programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
