77a11b492825a6f140de14fe1b112f2a76290082
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-02 08:58:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
      }
 
 		 programmer_hash[:yukihiro_matsumoto] = {}
-		 #programmer_hash[:yukihiro_matsumoto][:languages] = "C"
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
 		 programmer_hash
 
 
